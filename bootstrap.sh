d53f92ca2660f46dda6daeee4431fa563d7b0874
Luis Rampolla <33848069+rampollaluis@users.noreply.github.com>
2023-04-29 01:00:54 -0400

    symlinks for repo and user scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,20 +2,8 @@
 ln -sv ~/.dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 ln -sv ~/.dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 
-# convert all files in /bin directory to executables. These are the files that will be used to bootstrap and for the repo. NOT ones that will be transferred to bootstrapped system
-chmod +x ~/.dotfiles/bin/output-vscode-extensions
-chmod +x ~/.dotfiles/bin/dotfiles-precommit-hook
-chmod -c +x ~/.dotfiles/bin/install-vscode-extensions
-
 # Make a copy of the pre-commit hook file on the .git/hooks directory
-# idk if this is actually needed or cloning the repo already does it
 # cp ~/.dotfiles/bin/dotfiles-precommit-hook ~/.dotfiles/.git/hooks/pre-commit
-
-# create ~/bin folder and move all scripts in /scripts into there
-mkdir ~/bin
-ln -sv ~/.dotfiles/scripts/* ~/bin
-# make them all executables
-chmod +x ~/bin/brew-upgrade-monthly
 
 # install all vscode extensions
 "$HOME"/.dotfiles/bin/install-vscode-extensions
